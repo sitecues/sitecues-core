b4649c7715ab9bbe308f21d50a504ec85b0a0cd0
Alistair MacDonald <amacdonald@aisquared.com>
2014-01-17 11:45:34 -0500

    Javascript now loads ie6 and earlier css to make the fallback show up in the right place.

--- src/js/sitecues-fallback.js
+++ src/js/sitecues-fallback.js
@@ -19,7 +19,7 @@
         result = parseFloat( RegExp.$1 );
       }
     }
-    
+
     return result;
   }
 
@@ -47,213 +47,216 @@
         msgContainer,
         badgeImage,
         IEv = getIEv(),
-
         badge = document.getElementById('sitecues-badge'),
         fallback = document.getElementById('sitecuesFallback');
+
+    if (IEv <= 6) {
+      importCSS(window.sitecues.fallbackBasePath + 'css/sitecues-fallback-lteie6.css')
+    }
 
     if (badge === null){
       badge = createBadge();
       setBadgeStyle(badge);
     } else {
       badge.onclick = function(){
-          toggleFallbackModal()
-    }
-  };
-
-  if (fallback === null){
-    fallback = createFallback();
-  };
-
-  function setBadgeStyle(badge){
-    badgeImage = document.getElementById('sitecues-badge-image');
-
-    body.style.position = 'relative';
-    badge.style.cursor = 'pointer';
-    /*
-    Have to write out TRBL for IE6 as '!important'
-    is only supported if it is the last
-    declaration in a string
-    */
-    badge.style.margin.top = '0px !important';
-    badge.style.margin.right = '0px !important';
-    badge.style.margin.bottom = '0px !important';
-    badge.style.margin.left = '0px !important';
-        
-    badge.style.padding.top = '0px !important';
-    badge.style.padding.right = '0px !important';
-    badge.style.padding.bottom = '0px !important';
-    badge.style.padding.left = '0px !important';
-        
-    badge.style.border.top = '0px !important';
-    badge.style.border.right = '0px !important';
-    badge.style.border.bottom = '0px !important';
-    badge.style.border.left = '0px !important';
-
-    //badge.style.outline = '0px !important';
-    badge.style.position = ( (IEv >= 8) || (IEv == -1) ) ? 'fixed' : 'absolute';
-    badge.style.top = '5px';
-    badge.style.right = '20px';
-    badge.style.zIndex = '2147483645';
-        
-    badge.style.width = '150px';
-    badge.style.transition = 'opacity 500ms';
-    badge.style.boxSizing = 'content-box';
-
-    badgeImage.style.height = '25px';
-    badgeImage.style.width = '134px';
-    badgeImage.style.border = '2px solid #A3A3A3';
-    badgeImage.style.padding = '4px';
-    badgeImage.style.backgroundColor = 'white';
-    badgeImage.style.borderRadius = '4px';
-    badgeImage.style.boxShadow = '1px 1px 5px 0px rgba(10, 10, 10, .5)';
-  }
-
-  function createBadge(success){
-    var badge = document.createElement('div');
-    badge.id = 'sitecues-badge';
-
-    badge.sitecuesFallbackImage = sitecuesFallbackImage = document.createElement('img');
-    sitecuesFallbackImage.id = 'sitecues-badge-image';
-    sitecuesFallbackImage.src = window.sitecues.fallbackBasePath + 'img/sitecues-fallback-badge.png';
-    sitecuesFallbackImage.title = 'sitecues™ - fallback trigger.';
-    sitecuesFallbackImage.alt = 'sitecues™ fallback trigger.';
-
-    body.appendChild(badge);
-    badge.appendChild(sitecuesFallbackImage);
-
-    badge.onclick = function(){
-      toggleFallbackModal();
+        toggleFallbackModal()
+      }
     };
 
-    return badge;
-  };
+    if (fallback === null){
+      fallback = createFallback();
+    };
 
-  function createFallback(success) {
-    /* load fallback styles */
-    importCSS(window.sitecues.fallbackBasePath + 'css/sitecues-fallback.css');
+    function setBadgeStyle(badge){
+      badgeImage = document.getElementById('sitecues-badge-image');
 
-    /* Background mask for fallback message */
-    var fallback = mask = document.createElement('div');
-        mask.id = 'sitecuesFallback';
-        mask.className = 'close hide';
-    
-    body.appendChild(mask);
+      body.style.position = 'relative';
+      badge.style.cursor = 'pointer';
+      /*
+      Have to write out TRBL for IE6 as '!important'
+      is only supported if it is the last
+      declaration in a string
+      */
+      badge.style.margin.top = '0px !important';
+      badge.style.margin.right = '0px !important';
+      badge.style.margin.bottom = '0px !important';
+      badge.style.margin.left = '0px !important';
+          
+      badge.style.padding.top = '0px !important';
+      badge.style.padding.right = '0px !important';
+      badge.style.padding.bottom = '0px !important';
+      badge.style.padding.left = '0px !important';
+          
+      badge.style.border.top = '0px !important';
+      badge.style.border.right = '0px !important';
+      badge.style.border.bottom = '0px !important';
+      badge.style.border.left = '0px !important';
 
-    fallback.overlay = overlay = document.createElement('div');
-    overlay.className = 'overlay';
-    overlay.id = 'overlayMask';
+      //badge.style.outline = '0px !important';
+      badge.style.position = ( (IEv >= 8) || (IEv == -1) ) ? 'fixed' : 'absolute';
+      badge.style.top = '5px';
+      badge.style.right = '20px';
+      badge.style.zIndex = '2147483645';
+          
+      badge.style.width = '150px';
+      badge.style.transition = 'opacity 500ms';
+      badge.style.boxSizing = 'content-box';
 
-    overlay.style.height = getDocHeight() + '200px';
-      
-    if ( (IEv == 6) || (IEv == 7) || (IEv == 8) ){
-      overlay.style.height = getDocHeight();
-      overlay.style.opacity = '0.5';
-      overlay.style.filter = 'filter:alpha(opacity=50)';
-      overlay.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(Opacity=50)';
+      badgeImage.style.height = '25px';
+      badgeImage.style.width = '134px';
+      badgeImage.style.border = '2px solid #A3A3A3';
+      badgeImage.style.padding = '4px';
+      badgeImage.style.backgroundColor = 'white';
+      badgeImage.style.borderRadius = '4px';
+      badgeImage.style.boxShadow = '1px 1px 5px 0px rgba(10, 10, 10, .5)';
     }
 
-    mask.appendChild(overlay);
+    function createBadge(success){
+      var badge = document.createElement('div');
+      badge.id = 'sitecues-badge';
 
-    fallback.verticalOffset = vertOffset = document.createElement('div');
-    vertOffset.className = 'vertical-offset';
+      badge.sitecuesFallbackImage = sitecuesFallbackImage = document.createElement('img');
+      sitecuesFallbackImage.id = 'sitecues-badge-image';
+      sitecuesFallbackImage.src = window.sitecues.fallbackBasePath + 'img/sitecues-fallback-badge.png';
+      sitecuesFallbackImage.title = 'sitecues™ - fallback trigger.';
+      sitecuesFallbackImage.alt = 'sitecues™ fallback trigger.';
 
-    mask.appendChild(vertOffset);
-    
-    /* Modal div for fallback message */
-    fallback.modal = modal = document.createElement('div');
-    modal.id = 'sitecues-fallback-unsupported-browser';
-    modal.className = 'plainBoxWrap';
-    vertOffset.appendChild(modal);
-    
-    /* Used a table for the message */
-    fallback.table = messageHolder = document.createElement('div');
-    messageHolder.id = 'unsupported-browser-warning';
-    modal.appendChild(messageHolder);
+      body.appendChild(badge);
+      badge.appendChild(sitecuesFallbackImage);
 
-    fallback.contents = contentHolder = document.createElement('div');
-    contentHolder.id = 'content-holder';
+      badge.onclick = function(){
+        toggleFallbackModal();
+      };
 
-    fallback.buttons = btnHolder = document.createElement('div');
-    fallback.buttonDismiss = btnDismiss = document.createElement('div');
-    btnDismiss.id = 'dismiss-btn';
-    fallback.buttonExplore = btnExplore = document.createElement('div');
-    btnExplore.id = 'explore-btn';
+      return badge;
+    };
 
-    btnHolder.className = 'btn-group';
+    function createFallback(success) {
+      /* load fallback styles */
+      importCSS(window.sitecues.fallbackBasePath + 'css/sitecues-fallback.css');
 
-    messageHolder.appendChild(contentHolder);
-          
-    fallback.btn1 = btnDismissInt = document.createElement('a');
-           
-    btnDismissInt.type = 'button';
-    btnDismissInt.className = 'btn btn-default';
-    btnDismissInt.onclick = toggleFallbackModal;
-    btnDismissInt.innerHTML = 'Dismiss';
-              
-    btnDismiss.appendChild(btnDismissInt);
-
-    fallback.btn2 = btnExploreInt = document.createElement('a');
-              
-    btnExploreInt.type = 'button';
-    btnExploreInt.className = 'btn btn-default';
-    btnExploreInt.href = '//www.sitecues.com/compatibility.php';
-    btnExploreInt.target = '_blank';
-    btnExploreInt.innerHTML = 'Learn More';
-          
-    btnExplore.appendChild(btnExploreInt);
-
-    /* left table column - sitecues logo area */
-    fallback.logoHolder = logoHolder = document.createElement('div');
-    logoHolder.id = 'sitecues-logo-holder';
-
-    contentHolder.appendChild(logoHolder);
-
-    /* Left sitecues logo image */
-    fallback.sitecuesLogo = sitecuesLogo = document.createElement('img');
-    sitecuesLogo.id = 'sitecues-unsupported-browser-img';
-    sitecuesLogo.src = window.sitecues.fallbackBasePath + 'img/no-sitecues-support-warning.png';
-    sitecuesLogo.title = 'sitecues™ - unsupported browser image.';
-    sitecuesLogo.alt = 'Visit sitecues.com for more information.';
+      /* Background mask for fallback message */
+      var fallback = mask = document.createElement('div');
+          mask.id = 'sitecuesFallback';
+          mask.className = 'close hide';
       
-    /* Wrap the logo with an </a> for semantic html… */
-    fallback.sitecuesLogoLinkWrap = sitecuesLogoLinkWrap = document.createElement('a');
-    sitecuesLogoLinkWrap.className = 'logoWrapper';
-    sitecuesLogoLinkWrap.href = '//www.sitecues.com/';
-    sitecuesLogoLinkWrap.target = '_blank';
+      body.appendChild(mask);
 
-    logoHolder.appendChild(sitecuesLogoLinkWrap);
-    sitecuesLogoLinkWrap.appendChild(sitecuesLogo);
+      fallback.overlay = overlay = document.createElement('div');
+      overlay.className = 'overlay';
+      overlay.id = 'overlayMask';
 
-    /* Right column - message area */
-    fallback.messageContainer = msgContainer = document.createElement('div');
-    msgContainer.id = 'warning-message';
+      overlay.style.height = getDocHeight() + '200px';
+        
+      if ( (IEv == 6) || (IEv == 7) || (IEv == 8) ){
+        overlay.style.height = getDocHeight();
+        overlay.style.opacity = '0.5';
+        overlay.style.filter = 'filter:alpha(opacity=50)';
+        overlay.style.filter = 'progid:DXImageTransform.Microsoft.Alpha(Opacity=50)';
+      }
 
-    contentHolder.appendChild(msgContainer);
+      mask.appendChild(overlay);
 
-    fallback.displayedMsg = displayedMsg = '<h3>Our apologies!</h3><p>'+
-      '<span><strong>sitecues&trade;</strong> zoom &amp; speech tools </span>'+
-      '<span>'+msg+'</span>'+
-      '</p>';
+      fallback.verticalOffset = vertOffset = document.createElement('div');
+      vertOffset.className = 'vertical-offset';
 
-    msgContainer.innerHTML = displayedMsg;
+      mask.appendChild(vertOffset);
+      
+      /* Modal div for fallback message */
+      fallback.modal = modal = document.createElement('div');
+      modal.id = 'sitecues-fallback-unsupported-browser';
+      modal.className = 'plainBoxWrap';
+      vertOffset.appendChild(modal);
+      
+      /* Used a table for the message */
+      fallback.table = messageHolder = document.createElement('div');
+      messageHolder.id = 'unsupported-browser-warning';
+      modal.appendChild(messageHolder);
 
-    contentHolder.appendChild(btnHolder);
+      fallback.contents = contentHolder = document.createElement('div');
+      contentHolder.id = 'content-holder';
 
-    btnHolder.appendChild(btnDismiss);
-    btnHolder.appendChild(btnExplore);
+      fallback.buttons = btnHolder = document.createElement('div');
+      fallback.buttonDismiss = btnDismiss = document.createElement('div');
+      btnDismiss.id = 'dismiss-btn';
+      fallback.buttonExplore = btnExplore = document.createElement('div');
+      btnExplore.id = 'explore-btn';
 
-    return fallback;
-  };
+      btnHolder.className = 'btn-group';
 
-  function importCSS(src) {
-    var cssLink = document.createElement('link');
-    cssLink.rel = 'stylesheet';
-    cssLink.type = 'text/css';
-    cssLink.href = src;
-    head.appendChild(cssLink);
-  };
+      messageHolder.appendChild(contentHolder);
+            
+      fallback.btn1 = btnDismissInt = document.createElement('a');
+             
+      btnDismissInt.type = 'button';
+      btnDismissInt.className = 'btn btn-default';
+      btnDismissInt.onclick = toggleFallbackModal;
+      btnDismissInt.innerHTML = 'Dismiss';
+                
+      btnDismiss.appendChild(btnDismissInt);
 
- }
+      fallback.btn2 = btnExploreInt = document.createElement('a');
+                
+      btnExploreInt.type = 'button';
+      btnExploreInt.className = 'btn btn-default';
+      btnExploreInt.href = '//www.sitecues.com/compatibility.php';
+      btnExploreInt.target = '_blank';
+      btnExploreInt.innerHTML = 'Learn More';
+            
+      btnExplore.appendChild(btnExploreInt);
+
+      /* left table column - sitecues logo area */
+      fallback.logoHolder = logoHolder = document.createElement('div');
+      logoHolder.id = 'sitecues-logo-holder';
+
+      contentHolder.appendChild(logoHolder);
+
+      /* Left sitecues logo image */
+      fallback.sitecuesLogo = sitecuesLogo = document.createElement('img');
+      sitecuesLogo.id = 'sitecues-unsupported-browser-img';
+      sitecuesLogo.src = window.sitecues.fallbackBasePath + 'img/no-sitecues-support-warning.png';
+      sitecuesLogo.title = 'sitecues™ - unsupported browser image.';
+      sitecuesLogo.alt = 'Visit sitecues.com for more information.';
+        
+      /* Wrap the logo with an </a> for semantic html… */
+      fallback.sitecuesLogoLinkWrap = sitecuesLogoLinkWrap = document.createElement('a');
+      sitecuesLogoLinkWrap.className = 'logoWrapper';
+      sitecuesLogoLinkWrap.href = '//www.sitecues.com/';
+      sitecuesLogoLinkWrap.target = '_blank';
+
+      logoHolder.appendChild(sitecuesLogoLinkWrap);
+      sitecuesLogoLinkWrap.appendChild(sitecuesLogo);
+
+      /* Right column - message area */
+      fallback.messageContainer = msgContainer = document.createElement('div');
+      msgContainer.id = 'warning-message';
+
+      contentHolder.appendChild(msgContainer);
+
+      fallback.displayedMsg = displayedMsg = '<h3>Our apologies!</h3><p>'+
+        '<span><strong>sitecues&trade;</strong> zoom &amp; speech tools </span>'+
+        '<span>'+msg+'</span>'+
+        '</p>';
+
+      msgContainer.innerHTML = displayedMsg;
+
+      contentHolder.appendChild(btnHolder);
+
+      btnHolder.appendChild(btnDismiss);
+      btnHolder.appendChild(btnExplore);
+
+      return fallback;
+    };
+
+    function importCSS(src) {
+      var cssLink = document.createElement('link');
+      cssLink.rel = 'stylesheet';
+      cssLink.type = 'text/css';
+      cssLink.href = src;
+      head.appendChild(cssLink);
+    };
+
+   }
 
   function toggleFallbackModal() {
     var e = document.getElementById('sitecuesFallback');
