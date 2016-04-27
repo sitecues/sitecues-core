c51a7aabf7f1146702d1b37f9770f30debde9a99
Seth Holladay <me@seth-holladay.com>
2016-04-27 18:49:42 -0400

    Fix IE exception related to class inheritance.

--- task/build.js
+++ task/build.js
@@ -9,10 +9,15 @@ const
 function build() {
     return rollup.rollup({
             entry : 'lib/start.js',
-            plugins: [ 
+            plugins : [
                 babel({
-                    presets: 'es2015-rollup'
-                }) 
+                    plugins : [
+                        ['transform-es2015-classes', {loose: true}]
+                    ],
+                    presets : [
+                        'es2015-rollup'
+                    ]
+                })
             ]
         })
         .then((bundle) => {
