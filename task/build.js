21a2014fc09bcf7550eaa1f6d143c0f6d4368158
Seth Holladay <me@seth-holladay.com>
2016-08-02 05:03:03 -0400

    Fix TODO and lint errors.

--- task/build.js
+++ task/build.js
@@ -1,34 +1,34 @@
 'use strict';
 
-const
-    fs = require('fs'),
-    rollup = require('rollup'),
-    path = require('path'),
-    buildDir = require('build-dir'),
-    babel = require('rollup-plugin-babel');
+const fs = require('fs');
+const path = require('path');
+const { rollup } = require('rollup');
+const buildDir = require('build-dir');
+const babel = require('rollup-plugin-babel');
+const appName = require('../package.json').name;
 
-function build() {
-    return rollup.rollup({
-            entry : 'lib/start.js',
-            plugins : [
-                babel({
-                    plugins : [
-                        ['transform-es2015-classes', {loose: true}]
-                    ],
-                    presets : [
-                        'es2015-rollup'
-                    ]
-                })
-            ]
-        })
+const build = () => {
+    return rollup({
+        entry : 'lib/start.js',
+        plugins : [
+            babel({
+                plugins : [
+                    ['transform-es2015-classes', { loose : true }]
+                ],
+                presets : [
+                    'es2015-rollup'
+                ]
+            })
+        ]
+    })
         .then((bundle) => {
             return buildDir.prepare().then((dir) => {
                 return bundle.write({
-                        format : 'iife',
-                        banner : fs.readFileSync('./lib/polyfill.js', 'utf8'),
-                        dest   : path.join(dir.path, 'sitecues-fallback.js'),
-                        sourceMap : true
-                    })
+                    format    : 'iife',
+                    banner    : fs.readFileSync('./lib/polyfill.js', 'utf8'),
+                    dest      : path.join(dir.path, appName + '.js'),
+                    sourceMap : true
+                })
                     .then(() => {
                         // Move the temp dir to its permanent home and set up
                         // latest links.
@@ -36,6 +36,6 @@ function build() {
                     });
             });
         });
-}
+};
 
 module.exports = build;
